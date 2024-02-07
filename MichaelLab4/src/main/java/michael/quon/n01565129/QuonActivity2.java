1dea259e8cba8ba3860839d858cb6bd0217eef90
Michael-Quon <michaelquon08@gmail.com>
2024-02-07 18:19:53 -0500

    Made the second screen recieve the edit text info from the first screen. But it does not show up on the second screen. Added a log.d to check the logcat which shows that there is data being passed

--- MichaelLab4/src/main/java/michael/quon/n01565129/QuonActivity2.java
+++ MichaelLab4/src/main/java/michael/quon/n01565129/QuonActivity2.java
@@ -1,15 +1,22 @@
-// Michael Quon | N01565129 | CENG-258-0CB
 package michael.quon.n01565129;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class QuonActivity2 extends AppCompatActivity {
+public class QuonActivity2 extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_quon);
+
+        TextView dataTextView = findViewById(R.id.dataTextView);
+        // Retrieve the long data passed from the first activity
+        long userData = getIntent().getLongExtra("DATA_KEY", -1000);
+        dataTextView.setText(String.valueOf(userData)); // Display only the number without the prefix
+
+        // Log the received data
+        Log.d("QuonActivity2", "Received data: " + userData);
     }
 }
