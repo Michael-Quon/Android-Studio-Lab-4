46b419f9a030c6e636334e509215051e76eba27b
Michael-Quon <michaelquon08@gmail.com>
2024-02-07 13:20:27 -0500

    Set the intial comments for the java classes and the strings.xml

--- MichaelLab4/src/main/java/michael/quon/n01565129/MichaelActivity1.java
+++ MichaelLab4/src/main/java/michael/quon/n01565129/MichaelActivity1.java
@@ -1,10 +1,12 @@
+// Michael Quon | N01565129 | CENG-258-0CB
+
 package michael.quon.n01565129;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MichaelActivity1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ MichaelLab4/src/main/java/michael/quon/n01565129/QuonActivity2.java
@@ -0,0 +1,15 @@
+// Michael Quon | N01565129 | CENG-258-0CB
+package michael.quon.n01565129;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QuonActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
